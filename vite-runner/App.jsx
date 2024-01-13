1760f29f3ffa4b280ec78091041997444e270b9e
fuzhenling <zhenling.fu@hotmail.com>
2024-01-13 22:23:39 +0800

    feat:vite config

---
+++ vite-runner/App.jsx
@@ -0,0 +1,14 @@
+
+import React from './core/React.js';
+
+// vite 自动编译以下
+const App = <div>hh  <div>aaa</div> </div>
+
+
+
+// function App() {
+//     return App
+// }
+
+console.log(App);
+export default App
--- vite-runner/main.js
+++ vite-runner/main.js
@@ -1,8 +1,9 @@
 
+import React from './core/React.js';
 
 import ReactDOM from './core/ReactDom.js';
 
 import App from './App.jsx';
 
-
+// 暂时不支持function component
 ReactDOM.createRoot(document.querySelector('#root')).render(App)
